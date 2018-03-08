bc9711e0ff751e99e3a63f5cb731d8d05f911a8d
pengbo <962549206@qq>
2018-03-08 19:16:53 +0800

    函数

---
+++ 第七课-函数.go
@@ -0,0 +1,36 @@
+package main
+/* 函数是go里面的核心设计，使用func关键字声明
+  func funcname(input1 type,input2 type2..) (output1 type1,output2,type2..) {
+  	//这里是处理逻辑代码
+    	//返回多个值
+    	return value1, value2
+  }
+ */
+
+import (
+	"fmt"
+)
+/*  求最大值函数，接收两个参数，返回值是int类型 */
+func max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func SumAndProduct()
+
+func main() {
+	x := 3
+	y := 4
+	z := -5
+
+	max_xy := max(x,y) /*  调用函数max，传递x，y参数 */
+	max_xz := max(x,z)
+
+	fmt.Printf("max(%d,%d) = %d\n",x,y,max_xy) /* max(3,4) = 4 */
+	fmt.Printf("max(%d,%d) = %d\n",x,z,max_xz) /* max(3,-5) = 3 */
+	fmt.Printf("max(%d,%d) = %d\n",y,z,max(y,z)) /* max(4,-5) = 4; 也可以直接调用 */
+
+
+}
