ae5f0fd0005d9c204ee6631fc2be5b3e9759591b
casc1990 <962549206@qq.com>
2018-03-11 23:51:53 +0800

    接口interface

---
+++ 第十一课-接口interface.go
@@ -0,0 +1,14 @@
+package main
+//interface: 表示一组method的组合，我们通过interface来定义对象的一组行为
+//interface和struct一样，也是一种类型，使用type定义
+//interface类型定义了一组方法，如果某个对象实现了某个interface的所有方法，则此对象就实现了此接口
+//任意的类型都实现了空interface(我们这样定义：interface{})，也就是包含0个method的interface
+//interface可以被任意的对象实现.同理，一个对象可以实现任意多个interface
+import (
+	"fmt"
+)
+
+
+func main() {
+
+}
