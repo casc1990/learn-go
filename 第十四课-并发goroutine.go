b4b31f016ddad59d281734e9017599f7938b07f1
casc1990 <962549206@qq.com>
2018-03-17 19:08:24 +0800

    并发

--- 第十四课-并发goroutine.go
+++ 第十四课-并发goroutine.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime"
 	"strconv"
+	"time"
 )
 
 func say(s string) {
@@ -31,22 +32,25 @@ func fibonacci (n int, c chan int) {
 		c <- x  //向channel中发送数据，同时能存储多少，取决于channel的Buffered
 		x, y = y, x+y
 	}
-	close(c)  //关闭channel，关闭了就不能读取和接收了
+	close(c)  //关闭channel，关闭的信道会禁止数据流入, 是只读的
 }
 
-//channel的select
+//channel的select（select和switch一样,可处理一个或多个channel的发送与接收 ）
 func fibonacci1(c,quit chan int) {
 	x ,y := 1,1
-	for {
+	for {     //因为select执行完就会退出，使用for无限接收或读取数据
 		select {
-		case c <- x:
+		case c <- x:  //根据case处理多个或者同一个channel
 			x , y = y , x + y
-		case <- quit:
+		case <- quit:  //如果quit这个channel收到数据，程序退出
 			fmt.Println("quit..")
 			return
+		default:  //select的default语句
+			fmt.Println("done!")
 		}
 	}
 }
+
 
 func main() {
 	go say("hello world")  //通过go关键字，开一个新的goroutines执行
@@ -57,7 +61,7 @@ func main() {
 	a := []int{7,2,8,-9,4,0}
 	cl := make(chan int)  //定义channel时，也需要指定发送或者接受着的值的类型
 	cs := make(chan string) //定义接受string类型的channel
-	cf := make(chan interface{})
+	var cf chan int = make(chan int)  //也可以这样定义channel
 	fmt.Println(cl, cs, cf)  //0xc042084000 0xc042084060 0xc0420840c0
 	go sum(a[:len(a)/2],cl) //通过go关键字，开一个新的goroutines执行
 	go sum(a[len(a)/2:],cl)
@@ -68,7 +72,7 @@ func main() {
 	////channel的range和close
 	c := make(chan int,10)  //指定channel最多可以同时接收或者发送10个
 	go fibonacci(20,c)  //cap(c)等于10
-	for i := range c {
+	for i := range c {    //range不等到信道关闭是不会结束读取的,range会阻塞当前goroutine,产生死锁
 		fmt.Println("---:",i)
 		fmt.Println(i)  //打印channel里的数据（取数据）
 	}
@@ -78,9 +82,26 @@ func main() {
 	quit := make(chan int)
 	go func() {
 		for i :=0;i < 10; i++ {
-			fmt.Println(<- c)
+			fmt.Println(<- c1)  //接收channel数据
 		}
-		quit <- 0
+		quit <- 0  //想quit的channel发送数据
 	}()
 	fibonacci1(c1,quit)
+
+	//select的超时设置
+	c4 := make(chan  int)
+	o := make(chan bool)
+	go func() {
+		for {
+			select {
+			case v := <- c4:
+				fmt.Println(v)
+			case <- time.After(3 * time.Second):
+				fmt.Println("timeout..")
+				o <- true
+				break
+			}
+		}
+	}()
+	<- o
 }
